09092a10dc8a6e873240391124673b274cce27ea
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-04 11:53:47 -0500

    Integración de la funcionalidad crud en el controller

---
+++ src/main/java/com/milenyum_soft/bazar/controller/ClienteController.java
@@ -0,0 +1,60 @@
+package com.milenyum_soft.bazar.controller;
+
+import com.milenyum_soft.bazar.modelo.Cliente;
+import com.milenyum_soft.bazar.service.ClienteService;
+import com.milenyum_soft.bazar.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private IClienteService  clienteService;
+
+
+    @PostMapping("clientes/crear")
+    public String created(@RequestBody Cliente cliente) {
+        clienteService.createdClient( cliente );
+
+        return "Cliente creado exitosamente";
+
+    }
+
+    @GetMapping("clientes")
+    public List<Cliente> listClientes() {
+
+        return clienteService.listClientes();
+    }
+
+    @GetMapping( "clientes/{id_cliente}")
+    public Cliente getCliente(@PathVariable Long id_cliente) {
+        return clienteService.getCliente( id_cliente );
+    }
+
+    @DeleteMapping( "clientes/{id_cliente}")
+    public String deleteCliente(@PathVariable Long id_cliente) {
+
+        clienteService.deleteCliente( id_cliente );
+        return "Cliente eliminado exitosamente";
+    }
+
+    @PutMapping("clientes/editar/{id_cliente}")
+    public String editCliente(@PathVariable Long id_cliente, @RequestBody Cliente cliente) {
+
+        Cliente client1 = clienteService.getCliente( id_cliente );
+        String nombrecl= client1.getNombre();
+        String apellido= client1.getApellido();
+        String dni= client1.getDni();
+        System.out.println(client1.toString());
+
+        Cliente client2 = clienteService.updateCliente(cliente);
+        System.out.println(client2.toString());
+
+        return "Cliente editado exitosamente "  + "Cliente antiguo: " + nombrecl + " Cliente editado:  " + client2.toString();
+
+    }
+
+}
--- src/main/java/com/milenyum_soft/bazar/modelo/Venta.java
+++ src/main/java/com/milenyum_soft/bazar/modelo/Venta.java
@@ -2,10 +2,7 @@ package com.milenyum_soft.bazar.modelo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Entity
 public class Venta {
     @Id
