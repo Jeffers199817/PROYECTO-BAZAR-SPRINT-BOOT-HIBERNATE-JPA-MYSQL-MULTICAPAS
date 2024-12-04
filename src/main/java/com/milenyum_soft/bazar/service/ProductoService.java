c57cd1a212b0b4ee356c2c7cc3fb621d794fdd46
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-03 20:21:58 -0500

    Implementación del crud completo en el service

--- src/main/java/com/milenyum_soft/bazar/service/ProductoService.java
+++ src/main/java/com/milenyum_soft/bazar/service/ProductoService.java
@@ -1,7 +1,50 @@
 package com.milenyum_soft.bazar.service;
 
+import com.milenyum_soft.bazar.modelo.Producto;
+import com.milenyum_soft.bazar.repository.IProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductoService {
+public class ProductoService implements IProductoService{
+
+    @Autowired
+    private IProductoRepository productoRepository;
+
+    @Override
+    public Producto getProducto(Long codigo) {
+
+        return   productoRepository.getById(codigo);
+    }
+
+    @Override
+    public List<Producto> getProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public void createProducto(Producto producto) {
+        productoRepository.save(producto);
+    }
+
+    @Override
+    public void deleteProducto(Long codigo) {
+            productoRepository.deleteById(codigo);
+    }
+
+    @Override
+    public Producto updateProducto(Producto producto) {
+
+            Producto produc = this.getProducto(producto.getCodigo_producto());
+            produc.setNombre(producto.getNombre());
+            produc.setMarca(producto.getMarca());
+            produc.setCosto(producto.getCosto());
+            produc.setCantidad_disponible(producto.getCantidad_disponible());
+
+            this.createProducto(produc);
+
+        return produc ;
+    }
 }
