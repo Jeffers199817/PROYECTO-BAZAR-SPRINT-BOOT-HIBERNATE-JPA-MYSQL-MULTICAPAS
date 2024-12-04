9539153a639f7e379921696a7f73f46d78ffcd22
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2024-12-04 11:43:57 -0500

    Integración de de la la funcionalidad de crud cleintes

--- src/main/java/com/milenyum_soft/bazar/service/ClienteService.java
+++ src/main/java/com/milenyum_soft/bazar/service/ClienteService.java
@@ -1,7 +1,49 @@
 package com.milenyum_soft.bazar.service;
 
+import com.milenyum_soft.bazar.modelo.Cliente;
+import com.milenyum_soft.bazar.repository.IClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ClienteService {
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private IClienteRepository clienteRepository;
+    @Override
+    public void createdClient(Cliente cliente) {
+        clienteRepository.save(cliente);
+
+    }
+
+    @Override
+    public List<Cliente> listClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente getCliente(Long id) {
+        return  clienteRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteCliente(Long id) {
+
+        clienteRepository.deleteById(id);
+    }
+
+    @Override
+    public Cliente updateCliente(Cliente cliente) {
+
+        Cliente client = this.getCliente(cliente.getId_cliente());
+        client.setId_cliente(cliente.getId_cliente());
+        client.setNombre(cliente.getNombre());
+        client.setApellido(cliente.getApellido());
+        client.setDni(client.getDni());
+
+        this.createdClient(client);
+        return client;
+    }
 }
--- src/main/java/com/milenyum_soft/bazar/service/IClienteService.java
+++ src/main/java/com/milenyum_soft/bazar/service/IClienteService.java
@@ -1,4 +1,27 @@
 package com.milenyum_soft.bazar.service;
 
+import com.milenyum_soft.bazar.modelo.Cliente;
+
+import java.util.List;
+
 public interface IClienteService {
+
+
+    //CREAR
+    public void createdClient(Cliente cliente);
+
+    //TRAER LISTA
+    public List<Cliente> listClientes();
+
+    //TRAER CLIENTE
+    public Cliente getCliente(Long id);
+
+    //ELIMINAR CLIENTE
+    public void deleteCliente(Long id);
+
+    //EDITAR CLIENTE
+
+    public Cliente updateCliente(Cliente cliente);
+
+
 }
